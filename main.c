3499c2d6ec852a67dd4d5fd47715adf879623266
Dan O'Brien <d.j.obrien@uon.edu.au>
2019-04-02 23:43:15 +0000

    Turned capitalisation code into function 'capitalise' and encryption into function 'encrypt' - confirmed still working

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+void capitalise(char []);
+void encrypt(char []);
 
 int main() {
 
 char str[1024];
-int i;
 
 printf("Enter a string of characters: ");
 fgets(str, sizeof(str), stdin);
@@ -12,23 +13,32 @@ printf("\n");
 
 printf("Your original string: %s\n", str);
 
-// CAPITALISE string input
-for(i = 0; str[i] != '\0'; i++){ 
-    // If the letter at string position 'i' is lower case, subtract 32 and write it back to the same position (makes it a capital)
-    if(str[i] <= 'z' && str[i] >= 'a') str[i] = str[i] - 32;
-}
+capitalise(str);
 printf("Your capitalised string: %s\n", str);
 
 printf("Your encrypted string: ");
-
-for(i = 0; str[i] != '\0'; i++){
-    
-    if(str[i] <= 'Z' && str[i] >= 'A') printf("%c", str[i] + 1);
-    else printf("%c", str[i]);
-}
-	
+encrypt(str);
+printf("\n");	
 
 	
 	
 	return 0;
+}
+
+void capitalise(char string[]){
+    int i;
+    // CAPITALISE string input
+    for(i = 0; string[i] != '\0'; i++){ 
+        // If the letter at string position 'i' is lower case, subtract 32 and write it back to the same position (makes it a capital)
+        if(string[i] <= 'z' && string[i] >= 'a') string[i] = string[i] - 32;
+    }
+}
+
+void encrypt(char string[]){
+    int i;
+    for(i = 0; string[i] != '\0'; i++)
+    {
+        if(string[i] <= 'Z' && string[i] >= 'A') printf("%c", string[i] + 1);
+        else printf("%c", string[i]);
+    }
 }
