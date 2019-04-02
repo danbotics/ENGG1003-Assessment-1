f7715ecb7834da12dc13c5ab2254d5be8c01e7e4
Dan O'Brien <d.j.obrien@uon.edu.au>
2019-04-02 22:17:50 +0000

    Work you little bastard

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+
 int main() {
-	printf("Woo! You got the template working!\n");
+char str[1024];
+
+printf("Enter a string of characters: ");
+fgets(str, sizeof(str), stdin);
+printf("\n");
+
+printf("Your string: %c\n", (int)str);
+	
+	hkshdlfkshldkfjshl
+	
+	
 	return 0;
 }
