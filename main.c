19436707c0abd22bb00415f0f2f66b0d315df437
Dan O'Brien <d.j.obrien@uon.edu.au>
2019-04-02 23:55:20 +0000

    Started menu implementation

--- main.c
+++ main.c
@@ -2,10 +2,26 @@
 
 void capitalise(char []);
 void encrypt(char []);
+void decrypt(char []);
 
 int main() {
 
 char str[1024];
+char encrypted[1024];
+int key = 0;
+int choice = 0;
+
+// Starting menu
+
+printf("-----------------------------------\n");
+printf("ENGG1003 - Programming Assessment 1\n");
+printf("-----------------------------------\n");
+printf("\n");
+printf("Would you like to:\n");
+printf("[1] Encrypt some text\n");
+printf("[2] Decrypt some text\n");
+printf("Your choice: ");
+scanf(%d, &choice);
 
 printf("Enter a string of characters: ");
 fgets(str, sizeof(str), stdin);
@@ -36,7 +52,14 @@ void encrypt(char string[]){
     int i;
     for(i = 0; string[i] != '\0'; i++)
     {
-        if(string[i] <= 'Z' && string[i] >= 'A') printf("%c", string[i] + 1);
+        if(string[i] <= 'Z' && string[i] >= 'A') printf("%c", string[i] + key);
         else printf("%c", string[i]);
     }
 }
+
+void encrypt(char string[]){
+    int i;
+    for(i = 0; string[i] != '\0'; i++)
+    {
+        if(string[i] <= 'Z' && string[i] >= 'A') printf("%c", string[i] - key);
+        else printf("%c", string[i]);
