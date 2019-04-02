47ee620674717fa2204f0e4342d9771f411c509f
Dan O'Brien <d.j.obrien@uon.edu.au>
2019-04-02 23:03:18 +0000

    Rotation cipher completely working with user input - accepts spaces & special characters but only encrypts letters

--- main.c
+++ main.c
@@ -12,18 +12,20 @@ printf("\n");
 
 printf("Your original string: %s\n", str);
 
-for(i = 0; str[i] != '\0'; i++)
-{
+// CAPITALISE string input
+for(i = 0; str[i] != '\0'; i++){ 
+    // If the letter at string position 'i' is lower case, subtract 32 and write it back to the same position (makes it a capital)
     if(str[i] <= 'z' && str[i] >= 'a') str[i] = str[i] - 32;
 }
-
 printf("Your capitalised string: %s\n", str);
 
-//printf("Your encrypted string: ");
+printf("Your encrypted string: ");
 
-//for(i = 0; str[i] != NULL; i++){
-//    printf("%c", str[i] + 1);
-//}
+for(i = 0; str[i] != '\0'; i++){
+    
+    if(str[i] <= 'Z' && str[i] >= 'A') printf("%c", str[i] + 1);
+    else printf("%c", str[i]);
+}
 	
 
 	
