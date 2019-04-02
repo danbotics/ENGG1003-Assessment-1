854f4e5820d3b5af7c27d074cfa3307834af7049
Dan O'Brien <d.j.obrien@uon.edu.au>
2019-04-02 22:36:28 +0000

    Code successfully implementing BASIC rotation cipher with user input (works with spaces)

--- main.c
+++ main.c
@@ -2,15 +2,23 @@
 
 
 int main() {
+
 char str[1024];
+int i;
 
 printf("Enter a string of characters: ");
 fgets(str, sizeof(str), stdin);
 printf("\n");
 
-printf("Your string: %c\n", (int)str);
+printf("Your original string: %s\n", str);
+
+printf("Your encrypted string: ");
+
+for(i = 0; str[i] != NULL; i++){
+    printf("%c", str[i] + 1);
+}
 	
-	hkshdlfkshldkfjshl
+
 	
 	
 	return 0;
