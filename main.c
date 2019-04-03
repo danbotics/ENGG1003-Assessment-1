919d4d68cf03d2d05984c6c80aaff69af87febaf
Dan O'Brien <d.j.obrien@uon.edu.au>
2019-04-03 00:48:37 +0000

    Added user input prompt for cipher key

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ void capitalise(char []);
 void encrypt(char []);
 void decrypt(char []);
 
-int key = 1;
+int ekey = 1, dkey = 1;
 
 int main() {
 
@@ -19,6 +19,7 @@ printf("\n");
 printf("Would you like to:\n");
 printf("  [1] Encrypt some text\n");
 printf("  [2] Decrypt some text\n");
+printf("\n");
 printf("Your choice: ");
 scanf("%d", &choice);
 getchar();
@@ -29,6 +30,9 @@ switch(choice){
         printf("\n");
         printf("Enter the text to encrypt (it will be capitalised automatically): ");
         fgets(str, sizeof(str), stdin);
+        printf("Rotation cipher key: ");
+        scanf("%d", &ekey);
+        getchar();
         printf("\n");
 
         capitalise(str);
@@ -44,6 +48,9 @@ switch(choice){
         printf("\n");
         printf("Enter the encrypted text: ");
         fgets(encrypted, sizeof(encrypted), stdin);
+        printf("Rotation cipher key: ");
+        scanf("%d", &dkey);
+        getchar();
         printf("\n");
 
         printf("Decrypted: ");
@@ -69,7 +76,7 @@ void encrypt(char string[]){
     int i;
     for(i = 0; string[i] != '\0'; i++)
     {
-        if(string[i] <= 'Z' && string[i] >= 'A') printf("%c", string[i] + key);
+        if(string[i] <= 'Z' && string[i] >= 'A') printf("%c", string[i] + ekey);
         else printf("%c", string[i]);
     }
 }
@@ -78,7 +85,7 @@ void decrypt(char string[]){
     int i;
     for(i = 0; string[i] != '\0'; i++)
     {
-        if(string[i] <= 'Z' && string[i] >= 'A') printf("%c", string[i] - key);
+        if(string[i] <= 'Z' && string[i] >= 'A') printf("%c", string[i] - dkey);
         else printf("%c", string[i]);
     }
 }
