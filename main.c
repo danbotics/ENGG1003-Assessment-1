81cfa6eca6ddc6f690f782c5f471865f80f49972
Dan O'Brien <d.j.obrien@uon.edu.au>
2019-04-15 13:03:07 +0000

    main.c completed - needs more commenting for switchcase function but otherwise complete.

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-void capitalise(char string[],); // Prototype for function that is passed a string and capitalises and lower case letters
+void capitalise(char string[]); // Prototype for function that is passed a string and capitalises and lower case letters
 void rEncrypt(char string[], int key); // Protype for rotation (Caeser) cipher ENCRYPTION function
 void rDecrypt(char string[], int key); // Protype for rotation (Caeser) cipher DECRYPTION function
 void sEncrypt(char string[], char key[]); // Protype for substitution cipher ENCRYPTION function
-void sDecrypt(char string[], char key); // Protype for substitution cipher DECRYPTION function
+void sDecrypt(char string[], char key[]); // Protype for substitution cipher DECRYPTION function
 
 int rEkey = 1, rDkey = 1; // Declare and intialise variables to store the keys for encrypting and decrypting rotation ciphers
 char sEkey[26], sDkey[26]; //Declare arrays used to store user specified substitution cipher keys (26 letters in non-standard order)
 
+char inputText[1024]; //Array to store plain text input to be encrypted
+char encryptedOutput[sizeof(inputText) - 1]; // Array to store output of rEncrypt() function (encrypted text)
+char encryptedText[1024]; // Array to store encrypted text to be decrypted
+char decryptedOutput[sizeof(encryptedText) - 1]; // Array to store output of rDecrypt() function (decrypted text)
+
 int main()
 {
-    char inputText[1024]; //Array to store plain text input to be encrypted
-    char encryptedOutput[sizeof(inputText) - 1]; // Array to store output of rEncrypt() function (encrypted text)
-    char encryptedText[1024]; // Array to store encrypted text to be decrypted
-    char decryptedOutput[sizeof(EncryptedText) - 1]; // Array to store output of rDecrypt() function (decrypted text)
     int choice = 0; // Variable to store choice in menu system
     
     // Menu System for function selection
@@ -50,8 +51,7 @@ int main()
             printf("Your original text: %s\n", inputText); // Verify user input text (now also capitalised)
 
             rEncrypt(inputText, rEkey); // Call function to encrypt using rotation cipher - passed inputText array and integer key as arguments
-            printf("Your encrypted string: ");
-            printf("%s", encryptedOutput); // Print new encrypted text to console
+            printf("Your encrypted string: %s \n\n", encryptedOutput); // Print new encrypted text to console
             break; // Leave switch function
             
         case 2: // ENCRYPT USING SUBSTITUTION CIPHER
@@ -67,21 +67,20 @@ int main()
             printf("Cipher key text: %s\n\n", sEkey);  // Verify user input cipher key
 
             sEncrypt(inputText, sEkey); // Call function to encrypt using substitution cipher - passed inputText and key arrays as arguments
-            printf("Your encrypted string: ");
-            printf("%s \n\n", encryptedOutput);  // Print new encrypted text to console
+            printf("Your encrypted string: %s \n\n", encryptedOutput);  // Print new encrypted text to console
             break; // Leave switch function
             
         case 3: // DECRYPT USING ROTATION CIPHER
             printf("\n");
             printf("Enter the encrypted text: ");
-            fgets(encryptedText, sizeof(encrypted), stdin);
+            fgets(encryptedText, sizeof(encryptedText), stdin);
             printf("Rotation cipher key: ");
             scanf("%d", &rDkey);
             getchar();
             printf("\n");
 
-            rDecrypt(encryptedText);
-            printf("Decrypted: %s\n", decryptedOutput);
+            rDecrypt(encryptedText, rDkey);
+            printf("Decrypted: %s\n\n", decryptedOutput);
             break;
             
         case 4: // DECRYPT USING SUBSTITUTION CIPHER
@@ -92,11 +91,11 @@ int main()
             fgets(sDkey, sizeof(sDkey), stdin);
             printf("\n");
             sDecrypt(encryptedText, sDkey);
-            printf("Decrypted: %s\n", decryptedOutput);
+            printf("Decrypted: %s\n\n", decryptedOutput);
             break;
         
         default: // Default case catched any other option not covered by the previous cases
-            printf("That function hasn't been implemented yet - stay tuned!");
+            printf("\nThat function hasn't been implemented yet - stay tuned!\n\n");
         
     }
 
@@ -130,7 +129,7 @@ void rEncrypt(char string[], int key)
         // If array element 'i' is a capital letter, subtract 'A' from the element, add the key, calculate the result % 26 and add 'A' again
         // This shifts each letter by the rotation key, assigning the result to encryptedOutput[i] - the % 26 operation allows the digits to
         // wrap around if already at the end of the alphabet.
-        if(string[i] <= 'Z' && string[i] >= 'A') encryptedOutput[i] = (string[i] - 'A' + key) % 26 + 'A');
+        if(string[i] <= 'Z' && string[i] >= 'A') encryptedOutput[i] = ((string[i] - 'A' + key) % 26 + 'A');
         else encryptedOutput[i] = string[i]; // If array element is not a capital letter, send it straight to output unchanged
     }
 }
@@ -140,14 +139,14 @@ SUBSTITUTION ENCRYPTION FUNCTION - takes an array containing a plain text string
 capital letters in a non-standard order). The function inspects each element of the input string, converts the letter to a number between 0 and
 25 then outputs that numbered element in key string. The encrypted text is stored in another array, encryptedOutput[].
 */
-void sEncrypt(char string[], char sEkey)
+void sEncrypt(char string[], char key[])
 {
     int i;
     for(i = 0; string[i] != '\0'; i++)
     {
         if(string[i] <= 'Z' && string[i] >= 'A')
         {
-            encryptedOutput[i] = sEkey[string[i] - 'A'];
+            encryptedOutput[i] = key[string[i] - 'A'];
             
         } else encryptedOutput[i] = string[i];
     }
@@ -171,5 +170,25 @@ void rDecrypt(char string[], int key)
             else decryptedOutput[i] = (((string[i] - 'A' - key) + 26) + 'A'); // If input array element will be below 0, add 26 to make it a positive number
         }
         else decryptedOutput[i] = string[i]; // If input array element is NOT a letter, sent it straight to decryptedOutput[i] without modification
+    }
+}
+
+void sDecrypt(char string[], char key[])
+{
+    int i, position;
+    char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // This is what each decrypted element will refer to in order to return the original letter
+    char *positionPointer;
+    
+    for(i = 0; string[i] != '\0'; i++)
+    {
+        if(string[i] <= 'Z' && string[i] >= 'A'){
+            positionPointer = strchr(key, string[i]); // Search for the character string[i] in the key[] array and return a pointer to the first element found
+            
+            // Subtract the pointer address to the start of key[] from the address stored in *positionPointer - this gives the element position (0 - 25) where
+            // the character string[i] can be found in key[]
+            position = positionPointer - key; 
+            decryptedOutput[i] = alphabet[position]; // Outputs the specified position in the alphabet array - this will be the original letter for the given key.
+        }
+        else decryptedOutput[i] = string[i];
     }
 }
